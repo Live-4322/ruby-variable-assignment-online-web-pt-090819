999b78475f6f83d2015e47eaa06b5382b22deb7c
Charlie Oster <oster.charlie@yahoo.com>
2019-09-11 20:25:21 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,18 @@
  # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+ # give it the value of the string "Hello World
+def greeting
+  greeting = "Hello World"
+  puts greeting
+end
+
+
+def get_variable_from_file(file,variable)
+  file_scope = binding
+  file_scope.eval(file.read(file))
+  
+  begin 
+   return file_scope_variable_get(variable)
+  rescue NameError
+   raise NameError, "local variable #{variable} not defined in #{file}."
+ end
+end 
